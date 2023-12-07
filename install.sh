3a775acdf8697e9f1ecec9934e93dc1601917c18
aswann <103862054+aswann45@users.noreply.github.com>
2023-12-07 10:12:00 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+# to initialize repo on new system run:
+# curl -Lks https://github.com/aswann45/dot-files/install.sh | /bin/bash
+#
+# borrowed from durdn at https://bitbucket.org/durdn/cfg/src/master/.bin/install.sh
+# 
+git clone --bare https://github.com/aswann45/dot-files.git $HOME/.cfg
+function config {
+   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+}
+mkdir -p .config-backup
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi;
+config checkout
+config config status.showUntrackedFiles no
