c457bd1227ded709a10f6fa984d3a31a69c7f4db
aswann <aswann@protonmail.com>
2023-12-11 12:06:33 -0500

    add screen layout scripts

---
+++ .screenlayout/home_desk.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-1-1 --off --output DP-1-2 --mode 1920x1080 --pos 3840x-720 --rotate left --scale 2x2 --output DP-1-3 --off
