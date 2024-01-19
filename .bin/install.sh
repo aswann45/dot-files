308ac46f5d99ea29a2ce5f4986f9d2e627ed0411
aswann <103862054+aswann45@users.noreply.github.com>
2024-01-19 11:15:57 -0500

    Update install.sh
    fix curl url

--- .bin/install.sh
+++ .bin/install.sh
@@ -1,5 +1,5 @@
 # to initialize repo on new system run:
-# curl -Lks https://github.com/aswann45/dot-files/install.sh | /bin/bash
+# curl -Lks https://raw.githubusercontent.com/aswann45/dot-files/master/.bin/install.sh | /bin/bash
 #
 # borrowed from durdn at https://bitbucket.org/durdn/cfg/src/master/.bin/install.sh
 # 
